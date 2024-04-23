f5d0b8de85ca27f56d3e1cfdb243bdae90573c98
deekast <96024516+deekast@users.noreply.github.com>
2024-04-23 14:52:21 -0400

    lesson submit

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python3
-
 class Book:
-    pass
+    def __init__(self, title:str, page_count:int):
+        if type(page_count) != int:
+            print ("page_count must be an integer")
+        self.title = title
+        self._page_count = page_count
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+    @page_count.setter
+    def page_count(self, value):
+        if type(value) != int:
+            print ("page_count must be an integer")
+        self._page_count = value
+
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+
+    
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,28 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand:str, size:int):
+        if type(size) != int:
+            print("size must be an integer")
+        self.brand = brand
+        self.size = size
+        self.condition = "New" 
+
+    def cobble(self):
+        if self.condition.lower() == "old":
+            self.condition = "New"
+            print("The shoes have been restored to new condition.")
+        else:
+            print("Your shoe is as good as new!")
+
+    @property
+    def size(self):
+        return self._size
+
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            print ("size must be an integer")
+        self._size = value
+
